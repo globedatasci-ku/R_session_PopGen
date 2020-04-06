d44cf44795d226d26112b0f2823feadfa172df4e
IsoldevR <isolde.vanriemsdijk@outlook.com>
2020-04-06 12:09:30 +0200

    Included F statistics tutorial

---
+++ Notes.R
@@ -0,0 +1,5 @@
+
+#Recommendations for Pophelper installation
+Sys.setenv("R_REMOTES_NO_ERRORS_FROM_WARNINGS"=TRUE)
+#install pophelper package from GitHub
+remotes::install_github('royfrancis/pophelper')
